dfa60791b9b75e157d31d7b6e7506ed014c5da67
lean <l0958242580@icloud.com>
2023-08-09 14:48:44 +0000

    調整interface

--- internal/bandwidth/bandwidth.go
+++ internal/bandwidth/bandwidth.go
@@ -9,20 +9,22 @@ import (
 )
 
 func main() {
+	interfaceName := "lo"
 	for {
-		download1, upload1 := getNetworkStats()
+		download1, upload1 := getNetworkStats(interfaceName)
 		time.Sleep(5 * time.Second)
-		download2, upload2 := getNetworkStats()
+		download2, upload2 := getNetworkStats(interfaceName)
 
 		downloadRate := float64(download2-download1) / 5
 		uploadRate := float64(upload2-upload1) / 5
 
+		fmt.Printf("Interface: %s\n", interfaceName)
 		fmt.Printf("Download rate: %f MB/s\n", downloadRate/1024.0/1024.0)
 		fmt.Printf("Upload rate: %f MB/s\n", uploadRate/1024.0/1024.0)
 	}
 }
 
-func getNetworkStats() (download, upload uint64) {
+func getNetworkStats(interfaceName string) (download, upload uint64) {
 	data, err := os.ReadFile("/proc/net/dev")
 	if err != nil {
 		panic(err)
@@ -41,7 +43,7 @@ func getNetworkStats() (download, upload uint64) {
 		}
 
 		iface := fields[0]
-		if iface == "ens4:" {
+		if iface == interfaceName+":" {
 			download, _ = strconv.ParseUint(fields[1], 10, 64)
 			upload, _ = strconv.ParseUint(fields[9], 10, 64)
 			break
