2c2973356750db0fbfd5516060b0cd2968f2430a
lean <l0958242580@icloud.com>
2023-08-10 03:47:13 +0000

    優化pingtest的錯誤處理

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -110,33 +111,43 @@ func packetLossRate(host string) (float64, error) {
 	return 0, errors.New("找不到封包丟失率")
 }
 
+
+const maxRetries = 3
 // pingTest 函數，執行 ping 測試
 func pingTest(host string, count int) ([]float64, error) {
-	cmd := exec.Command("ping", "-c", strconv.Itoa(count), host)
-	output, err := cmd.Output()
-	if err != nil {
-		return nil, err
-	}
+	for i := 0; i < maxRetries; i++ {
+		cmd := exec.Command("ping", "-c", strconv.Itoa(count), host)
+		output, err := cmd.CombinedOutput() // 獲取 STDOUT 和 STDERR
 
-	lines := strings.Split(string(output), "\n")
-	var pingResults []float64
-	for _, line := range lines {
-		if strings.Contains(line, "time=") {
-			timePart := strings.Split(line, "time=")[1]
-			timePart = strings.Split(timePart, " ")[0]
-			timeVal, err := strconv.ParseFloat(timePart, 64)
-			if err != nil {
-				return nil, err
+		if err == nil {
+			lines := strings.Split(string(output), "\n")
+			var pingResults []float64
+			for _, line := range lines {
+				if strings.Contains(line, "time=") {
+					timePart := strings.Split(line, "time=")[1]
+					timePart = strings.Split(timePart, " ")[0]
+					timeVal, err := strconv.ParseFloat(timePart, 64)
+					if err != nil {
+						return nil, fmt.Errorf("ping 輸出時發生錯誤: %v", err)
+					}
+					pingResults = append(pingResults, timeVal)
+				}
 			}
-			pingResults = append(pingResults, timeVal)
+			if len(pingResults) == 0 {
+				return nil, errors.New("找不到 ping 結果")
+			}
+			return pingResults, nil
+		} else {
+			fmt.Printf("Ping 測試失敗: %v\n", err) // 輸出錯誤訊息
+			if i < maxRetries-1 {
+				fmt.Println("重新ping", host, "中") 
+				// 等一秒後再重試
+				time.Sleep(1 * time.Second)
+			}
 		}
 	}
 
-	if len(pingResults) == 0 {
-		return nil, errors.New("找不到 ping 結果")
-	}
-
-	return pingResults, nil
+	return nil, fmt.Errorf("ping 命令失敗達到上限")
 }
 
 func abs(x float64) float64 {
